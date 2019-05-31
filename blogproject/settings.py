b8911f3499d12c4467baf03e991c340cf311ef08
IronWingS <953890297@qq.com>
2019-05-31 10:51:57 +0800

    test

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/shanghai'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
